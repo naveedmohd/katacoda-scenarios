305355d4828b259e24f70004a935c669e1810def
Mohammad Naveed <mohammad.naveed@mohammads-mbp-2.attlocal.net>
2020-07-01 15:34:31 -0700

    fix the script

--- installmysql/createdb.sh
+++ installmysql/createdb.sh
@@ -1,1 +1,1 @@
-docker run -d --name mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=studentdb -e MYSQL_USER=demo -e MYSQL_PASSWORD=password
+docker run -d --name mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=studentdb -e MYSQL_USER=demo -e MYSQL_PASSWORD=password mysql
