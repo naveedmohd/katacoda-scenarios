57d7d8e2c4fc82dc8fdbe0087c964c7572e4f7fc
Mohammad Naveed <mohammad.naveed@mohammads-mbp-2.attlocal.net>
2020-08-27 21:55:56 -0700

    Add Step3 for scripts

---
+++ py-scripts/assets/list-functions.py
@@ -0,0 +1,31 @@
+def getTotalElements(listElements):
+    '''
+    The function takes list as input parameter.
+    The function counts elements present in the list.
+    Finally, it prints the total count of elements.
+    '''
+    count=0
+    for i in enumerate(listElements):
+        count=count+1
+    return('Total Elements present in LIST are: ' , count)
+
+total=0
+
+def calculateListSum(listElements,total):
+    '''
+    The function takes list as input parameter.
+    The function calculates the sum from all elements of an integer list.
+    Finally, it prints the sum at the end.
+    '''
+    for i ,element in enumerate(listElements):
+        total += element
+    return total
+
+
+listElements=[1,2,3,4,5,6,7,8,9,10]
+
+print(getTotalElements(listElements))
+
+total=calculateListSum(listElements,total)
+
+print('Sum of elements in list: " , total)
